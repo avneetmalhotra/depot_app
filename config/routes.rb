e4fea6323bcb4154ae4227906c99fc980f0e3a43
Avneet Singh Malhotra <avneet@vinsol.com>
2017-11-01 14:38:14 +0530

    Catalog Display

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # get 'store/index'
+  # ^this is replaces with below line
+  # making store/index as root url of the website
+  root 'store#index', as: 'store_index'
+
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
