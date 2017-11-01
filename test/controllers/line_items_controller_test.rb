14b353cda473bbd74a42c6274adb7afbab6ad116
Avneet Singh Malhotra <avneet@vinsol.com>
2017-11-01 19:48:28 +0530

    Chapter 10

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -23,9 +23,8 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     follow_redirect!
 
-    assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select 'li', 'Rails, Angular, Postgres, and Bootstrap'
-    # assert_redirected_to line_item_url(LineItem.last)
+    assert_select 'h2', 'Your Cart'
+    assert_select 'td', "Rails, Angular, Postgres, and Bootstrap"
   end
 
   test "should show line_item" do
@@ -39,7 +38,8 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update line_item" do
-    patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+    # patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+    patch line_item_url(@line_item), params: { line_item: { product_id: @line_item.product_id } }
     assert_redirected_to line_item_url(@line_item)
   end
 
