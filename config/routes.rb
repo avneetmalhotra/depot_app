e167dc27c3f900baba0bd4560c75e4f50638e8ff
Avneet Singh Malhotra <avneet@vinsol.com>
2017-11-02 18:24:37 +0530

    chapter 12

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items
   resources :carts
   # get 'store/index'
@@ -6,6 +7,8 @@ Rails.application.routes.draw do
   # making store/index as root url of the website
   root 'store#index', as: 'store_index'
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
