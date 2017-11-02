ec59e143f4e0ac41a552954143f5038aa45bcefe
Avneet Singh Malhotra <avneet@vinsol.com>
2017-11-02 19:49:34 +0530

    chapter 13

---
+++ test/mailers/order_mailer_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class OrderMailerTest < ActionMailer::TestCase
+  test "received" do
+    mail = OrderMailer.received(orders(:one))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /1 x Rails, Angular, Postgres, and Bootstrap9/, mail.body.encoded
+  end
+
+  test "shipped" do
+    mail = OrderMailer.shipped(orders(:one))
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /<td>1&times;<\/td>\s*<td>Rails, Angular, Postgres, and Bootstrap<\/td>/,
+      mail.body.encoded
+  end
+
+end
